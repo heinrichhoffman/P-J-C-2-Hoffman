ae2c8a8d7142484fb7ef7c7c569979f4eaba83ba
heinrichhoffman <hoffmanheinrich@gmail.com>
2025-01-19 22:01:22 -0300

    Projeto inicial Validador Triângulo

---
+++ exercício_1_do_triângulo.py
@@ -0,0 +1,8 @@
+from validador_de_triangulo import perimetro 
+
+a = int(input("Insira o lado A"))
+b = int(input("Insira o lado B"))
+c = int(input("Insira o lado C"))
+
+resultado = perimetro(a,b,c)
+print(resultado)
